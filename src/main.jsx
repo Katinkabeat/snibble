f04c73318e707d6e426fe3f4c99a641a06f2a368
Katinkabeat <tracey8008@hotmail.com>
2026-05-21 15:18:04 -0300

    Wrap app in SQErrorBoundary (c117)

--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,16 @@ import ReactDOM from 'react-dom/client'
 import { Toaster } from 'react-hot-toast'
 import App from './App.jsx'
 import { ThemeProvider } from './contexts/ThemeContext.jsx'
+import { SQErrorBoundary } from '../../rae-side-quest/packages/sq-ui/index.js'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ThemeProvider>
-      <App />
-      <Toaster position="top-center" toastOptions={{ duration: 1800 }} />
-    </ThemeProvider>
+    <SQErrorBoundary label="snibble">
+      <ThemeProvider>
+        <App />
+        <Toaster position="top-center" toastOptions={{ duration: 1800 }} />
+      </ThemeProvider>
+    </SQErrorBoundary>
   </React.StrictMode>
 )
