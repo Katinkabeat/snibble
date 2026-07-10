75907c616729e9142f84a3ae3ec2a348ecb6baa5
Katinkabeat <tracey8008@hotmail.com>
2026-07-10 13:57:16 -0300

    install global error reporting to #error-log (c266 phase 1)

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,15 @@ import ReactDOM from 'react-dom/client'
 import { Toaster } from 'react-hot-toast'
 import App from './App.jsx'
 import { ThemeProvider } from './contexts/ThemeContext.jsx'
-import { SQErrorBoundary } from '../../rae-side-quest/packages/sq-ui/index.js'
+import { SQErrorBoundary, installGlobalErrorReporting } from '../../rae-side-quest/packages/sq-ui/index.js'
 import './index.css'
+
+// Report uncaught errors + unhandled rejections + render crashes to #error-log (c266).
+installGlobalErrorReporting({
+  game: 'snibble',
+  reportUrl: `${import.meta.env.VITE_SUPABASE_URL}/functions/v1/sq-report-client-error`,
+  anonKey: import.meta.env.VITE_SUPABASE_ANON_KEY,
+})
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
